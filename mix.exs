ce3582854353f1de571da9c10c3d9b12efa1e6f0
Ziyan Junaideen <ziyan@jdeen.com>
2026-04-21 12:58:10 +0530

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExDisposable.MixProject do
   def project do
     [
       app: :ex_disposable,
-      version: "0.4.0",
+      version: "0.5.0",
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
